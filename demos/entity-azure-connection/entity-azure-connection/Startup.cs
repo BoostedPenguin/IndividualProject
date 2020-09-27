c0743380d8d445ca37782707c06c6058de2ba485
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2020-09-27 14:12:25 +0200

    Update Startup.cs

--- demos/entity-azure-connection/entity-azure-connection/Startup.cs
+++ demos/entity-azure-connection/entity-azure-connection/Startup.cs
@@ -46,7 +46,7 @@ namespace entity_azure_connection
 
 
             services.AddDbContext<BloggingContext>(options => {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultAzureConnection"));
+                options.UseSqlServer("Server=tcp:individualproject.database.windows.net,1433;Initial Catalog=IndividualProject;Persist Security Info=False;User ID=penguinmaster;Password=bh4aebszmYFKj7D;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             });
 
             services.AddControllers();
