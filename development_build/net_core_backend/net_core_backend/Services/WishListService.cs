3e3ec1743c2bd10a33388ba1c2a9bb7268f63d4d
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2020-10-28 23:14:47 +0100

    Wishlist scaffolding

---
+++ development_build/net_core_backend/net_core_backend/Services/WishListService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using net_core_backend.Context;
+using net_core_backend.Models;
+using net_core_backend.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_backend.Services
+{
+    public class WishListService : DataService<WishList>, IWishListService
+    {
+        private readonly IContextFactory contextFactory;
+        private readonly IHttpContextAccessor httpContext;
+
+        public WishListService(IContextFactory _contextFactory, IHttpContextAccessor httpContextAccessor) : base(_contextFactory)
+        {
+            contextFactory = _contextFactory;
+            httpContext = httpContextAccessor;
+        }
+    }
+}
