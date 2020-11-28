c3b77efd522e47a6b6e8ff1da516b0168c626207
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2020-11-28 16:07:18 +0100

    Reviewed navigation bar, refined search menu with finer css, create a suggestions component

---
+++ development_build/vue_frontend/src/components/Suggestions.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="main">
+    <div class="container-fluid">
+      <div class="row mt-5">
+
+
+        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-12 col-12 pt-3" v-for="index in 8" :key="index">
+
+          <div class="card">
+            <a class="stretched-link text-decoration-none" href="#">
+              <img
+                src="../assets/EiffelTower.jpg"
+                class="card-img-top  gallery-cover"
+                alt="..."
+              />
+            </a>
+            
+            <div class="card-body">
+              <h5 class="card-title">
+                <div class="d-flex justify-content-between">
+                  <div>Name</div>
+                  <div>
+                    <p class="out-of-stock">Rating?</p>
+                  </div>
+                </div>
+              </h5>
+              <div class="card-text d-flex justify-content-between">
+                <div>Description</div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background-color: var(--primary-variant);
+}
+
+/* Card body control  */
+.gallery-cover {
+  width: 100%;
+  min-height: 100px;
+  height: 20vw;
+  object-fit: cover;
+}
+
+@media (max-width: 768px) {
+  .gallery-cover {
+    height: 60vw;
+  }
+}
+
+.card {
+    border: 1px;
+      box-shadow: 1px 2px 7px 6px rgba(0,0,0,0.25);
+
+
+  transition: 0.5s;
+}
+
+.container-fluid {
+  padding-right: 50px;
+  padding-left: 50px;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
--- development_build/vue_frontend/src/views/Home.vue
+++ development_build/vue_frontend/src/views/Home.vue
@@ -1,35 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-
-    <!-- Check that the SDK client is not currently loading before accessing is methods -->
-    <div v-if="!$auth.loading">
-      <!-- show login when not authenticated -->
-      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
-      <!-- show logout when authenticated -->
-      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
-    </div>
+  <div class="main">
+    <SearchMenu />
+    <Suggestions />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import SearchMenu from "../components/SearchMenu";
+import Suggestions from "../components/Suggestions";
 
 export default {
-  name: "Home",
   components: {
-  },
-  methods: {
-    // Log the user in
-    login() {
-      this.$auth.loginWithRedirect();
-    },
-    // Log the user out
-    logout() {
-      this.$auth.logout({
-        returnTo: window.location.origin
-      });
-    }
+    SearchMenu, Suggestions
   }
 };
 </script>
+
+<style>
+</style>
